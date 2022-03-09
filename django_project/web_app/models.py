179ab4b3b0d3e4f5378e9251468a559893488d40
amazingaarati@gmail.com <amazingaarati@gmail.com>
2022-03-09 23:23:20 +0545

    Post model created

--- django_project/web_app/models.py
+++ django_project/web_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model): #eachh class going to be each table in the database
+    title = models.CharField(max_length = 100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default = timezone.now) 
+    author = models.ForeignKey(User, on_delete = models.CASCADE)
+
+
+    # auto_now_add you cant update the value of the post
+    # to manage the value used the default as pass timezone as the value 
+    # this function wil take the time from the current time zone
+    # we need to run the migration to update the database in Django 
+
+
